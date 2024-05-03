5ec50b369ebc65dfbf1e8a8943b67e5e1b61041b
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-05-03 16:05:15 +0900

    first commit

---
+++ src/main/java/com/example/demo/BaeminPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeminPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeminPracticeApplication.class, args);
+	}
+
+}
